cc0da0af01dc903c254ec9c43505953f60a7246d
vinay2105 <kvinay21505@gmail.com>
2024-11-06 10:32:08 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 class Linear_Regression():
